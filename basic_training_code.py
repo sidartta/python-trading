cc622a76a34fde2adebd4c83f298430c52b5d19c
sidartta <sidartta@users.noreply.github.com>
2020-04-10 19:46:43 -0400

    Code clean up & optimization
    Signed-off-by: sidartta <sidartta@users.noreply.github.com>

--- basic_training_code.py
+++ basic_training_code.py
@@ -1,27 +1,30 @@
 # Module & Libraries Import
+from datetime import date, timedelta
+from time import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from pandas.plotting import scatter_matrix
 from pandas_datareader import data as pdr
-from datetime import date
-from time import time
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 
 # Variables instantiation
+yrs = 3
 start_time = time()
-start_date = date(2015, 1, 1)
-tickers = ['AAPL', 'C', 'GS', 'AMZN']
+start_date = date.today() - timedelta(365 * yrs)
+end_date = date.today()
+tickers = ['AAPL', 'C', 'GS', 'BBD-B.TO']
 
 
 # Function to get tickers data from Yahoo! Finance
-def get(tickers, startdate, enddate=None, r='D'):
+def get(tick, startdate, enddate=None, r='D'):
     def stock_data(ticker):
-        tick = pdr.get_data_yahoo(ticker, start=startdate, end=enddate)
+        tk = pdr.get_data_yahoo(ticker, start=startdate, end=enddate)
         if r is not None:
-            tick = tick.resample(rule=r, axis=0, closed='right').mean()
-        return tick.dropna()
+            tk = tk.resample(rule=r, axis=0, closed='right').mean()
+        return tk.dropna()
 
-    datas = map(stock_data, tickers)
+    datas = map(stock_data, tick)
     return pd.concat(datas, keys=tickers, names=['Ticker', 'Date'])
 
 
@@ -36,11 +39,11 @@ pct_chg = pd.DataFrame()
 for x in tickers:
     pct_chg[x] = all_data.loc[(x, 'Close')].pct_change(fill_method='ffill')
 pct_chg_log = np.log(pct_chg)
-cum_return = (1 + pct_chg).cumprod()
 
 # Basic stocks plots
 pct_chg.hist(bins=50, sharex=True)
 scatter_matrix(pct_chg, diagonal='kde', alpha=0.1)
+plt.tight_layout()
 plt.show()
 ###################################################
 print("--- %s seconds ---" % (time() - start_time))
