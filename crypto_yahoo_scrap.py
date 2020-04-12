59f797411957e9c930d44089eedff09347d93183
sidartta <sidartta@users.noreply.github.com>
2020-04-12 15:02:57 -0400

    First release
    Signed-off-by: sidartta <sidartta@users.noreply.github.com>

---
+++ crypto_yahoo_scrap.py
@@ -0,0 +1,42 @@
+# Module & Libraries Import
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+names = []
+prices = []
+changes = []
+percentChanges = []
+marketCaps = []
+totalVolumes = []
+circulatingSupplys = []
+
+for i in range(0, 10):
+    CryptoCurrenciesUrl = "https://finance.yahoo.com/cryptocurrencies"
+    r = requests.get(CryptoCurrenciesUrl)
+    data = r.text
+    soup = BeautifulSoup(data)
+    for listing in soup.find_all('tr', attrs={'class': 'simpTblRow'}):
+        for name in listing.find_all('td', attrs={'aria-label': 'Name'}):
+            names.append(name.text)
+        for price in listing.find_all('td', attrs={'aria-label': 'Price (Intraday)'}):
+            prices.append(price.find('span').text)
+        for change in listing.find_all('td', attrs={'aria-label': 'Change'}):
+            changes.append(change.text)
+        for percentChange in listing.find_all('td', attrs={'aria-label': '% Change'}):
+            percentChanges.append(percentChange.text)
+        for marketCap in listing.find_all('td', attrs={'aria-label': 'Market Cap'}):
+            marketCaps.append(marketCap.text)
+        for totalVolume in listing.find_all('td', attrs={'aria-label': 'Total Volume All Currencies (24Hr)'}):
+            totalVolumes.append(totalVolume.text)
+        for circulatingSupply in listing.find_all('td', attrs={'aria-label': 'Circulating Supply'}):
+            circulatingSupplys.append(circulatingSupply.text)
+
+crypto = pd.DataFrame({'Price': prices,
+                       'Change': changes,
+                       '% Change': percentChanges,
+                       'Market Cap': marketCaps,
+                       'Total Volume': totalVolumes,
+                       'Circulating Supply': circulatingSupplys},
+                      index=names)
+print(crypto)
